db230a0682e63f208bcd557cc6fc17d1ce06a8b2
xiuwei <xiuwei@DESKTOP-CIM4AC6>
2022-10-15 22:20:15 -0700

    加入專案檔案。

---
+++ Exec_DeclareString_36/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareString_36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            string path = "C:\\temp\\a.jpg";
+            Console.WriteLine("Q1: "+path);
+
+            //Q2
+            string value = "我是一個\"好學生\".";
+            Console.WriteLine("Q2: " + value);
+            //Q3
+            string value2 = "我是第一行\n我是第二行";
+            Console.WriteLine("Q3: "+value2);
+
+		}
+    }
+}
